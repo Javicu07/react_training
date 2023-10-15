5541f17566f778f8a651afdf5ff83969015b09a0
Javicu07 <javicu89@gmail.com>
2023-10-15 17:47:54 +0200

    151006

--- trainee_1/src/App.jsx
+++ trainee_1/src/App.jsx
@@ -1,26 +1,18 @@
 import { useEffect, useState } from 'react'
+import './App.css'
+import { getRandomFact } from './services/facts'
 
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMAGE_URL = `https://catass.com/cat/says/${firstWord}?size=50&color=red&json=true`
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
 export function App () {
   const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
 
+  // efecto para cargar cita al renderizar la página
   // el 'fetch' no se puede hacer fuera del 'useEffect' porque se hace un loop infinito
   useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(res => res.json())
-      .then(data => {
-        const { fact } = data
-        setFact(fact)
-
-        // Con '.slice' y '.join' se puede recoger el # de palabras que queramos, si queremos solamente
-        // la primera palabra pudiera bastar con "fact.split(' ')[0]" y más directo sería usar
-        // "fact.split(' ', 3).join(' ')" y nos quedamos con las 3 primeras palabras
-        const firstWord = fact.split(' ').slice(0, 1).join(' ')
-        console.log(firstWord)
-      })
+    getRandomFact().then(setFact) // --> es lo mismo que ".then(newFact => setFact(newFact))"
   }, []) // en '[]' se ponen las depencias que pueden variar, en este caso 'setFact' no es necesario
 
   /*
@@ -36,11 +28,37 @@ export function App () {
     }, [])
   */
 
+  // efecto para recuperar la imagen cada vez que tengamos una nueva cita
+  useEffect(() => {
+    if (!fact) return
+    // Con '.slice' y '.join' se puede recoger el # de palabras que queramos, si queremos solamente
+    // la primera palabra pudiera bastar con "fact.split(' ')[0]" y más directo sería usar
+    // "fact.split(' ', 3).join(' ')" y nos quedamos con las 3 primeras palabras
+    const firstWord = fact.split(' ', 1).join(' ')
+    console.log(firstWord)
+
+    fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response
+        setImageUrl(url)
+      })
+  }, [fact])
+
+  const handleClick = async () => {
+    const newFact = await getRandomFact()
+    setFact(newFact)
+  }
+
   return (
     <main>
       <h1>App de Gatitos</h1>
-      {fact && <p>{fact}</p>}
-      {imageUrl && <img src={imageUrl} alt={`Image extracted using the first word from ${fact}`} />}
+
+      <button onClick={handleClick}>Get New Fact</button>
+      <section>
+        {fact && <p>{fact}</p>}
+        {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first word for ${fact}`} />}
+      </section>
     </main>
   )
 }
