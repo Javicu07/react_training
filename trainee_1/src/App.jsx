dd400877ef8f08e71bdf6041c69eca62cd52be53
Javicu07 <javicu89@gmail.com>
2023-10-15 18:20:50 +0200

    151007

--- trainee_1/src/App.jsx
+++ trainee_1/src/App.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { getRandomFact } from './services/facts'
+import { useCatImage } from './hooks/useCatImage'
 
 // const CAT_ENDPOINT_IMAGE_URL = `https://catass.com/cat/says/${firstWord}?size=50&color=red&json=true`
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
 export function App () {
   const [fact, setFact] = useState()
-  const [imageUrl, setImageUrl] = useState()
+  const { imageUrl } = useCatImage({ fact })
 
   // efecto para cargar cita al renderizar la página
   // el 'fetch' no se puede hacer fuera del 'useEffect' porque se hace un loop infinito
@@ -27,23 +28,6 @@ export function App () {
         getRandomFact()
     }, [])
   */
-
-  // efecto para recuperar la imagen cada vez que tengamos una nueva cita
-  useEffect(() => {
-    if (!fact) return
-    // Con '.slice' y '.join' se puede recoger el # de palabras que queramos, si queremos solamente
-    // la primera palabra pudiera bastar con "fact.split(' ')[0]" y más directo sería usar
-    // "fact.split(' ', 3).join(' ')" y nos quedamos con las 3 primeras palabras
-    const firstWord = fact.split(' ', 1).join(' ')
-    console.log(firstWord)
-
-    fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
-      .then(res => res.json())
-      .then(response => {
-        const { url } = response
-        setImageUrl(url)
-      })
-  }, [fact])
 
   const handleClick = async () => {
     const newFact = await getRandomFact()
---
+++ trainee_1/src/hooks/useCatImage.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react'
+
+export function useCatImage ({ fact }) {
+  const [imageUrl, setImageUrl] = useState()
+
+  // efecto para recuperar la imagen cada vez que tengamos una nueva cita
+  useEffect(() => {
+    if (!fact) return
+    // Con '.slice' y '.join' se puede recoger el # de palabras que queramos, si queremos solamente
+    // la primera palabra pudiera bastar con "fact.split(' ')[0]" y más directo sería usar
+    // "fact.split(' ', 3).join(' ')" y nos quedamos con las 3 primeras palabras
+    const firstWord = fact.split(' ', 1).join(' ')
+    console.log(firstWord)
+
+    fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response
+        setImageUrl(url)
+      })
+  }, [fact])
+
+  return { imageUrl }
+} // { imageUrl: "https://..." }
