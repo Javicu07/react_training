9d89a8ca37080f727ad6a912435441de3b024753
Javicu07 <javicu89@gmail.com>
2023-10-16 21:16:44 +0200

    adding_style

--- 00-hello-world/src/App.jsx
+++ 00-hello-world/src/App.jsx
@@ -1,7 +1,21 @@
+import './App.css'
+
 export function App () {
-    return(
+  return(
+    <article>
+      <header>
+        <img alt="squirrel avatar" src="https://cdn.pixabay.com/photo/2016/03/31/20/31/amazed-1295833_1280.png" />
         <div>
-            <h1>Twitter Card</h1>
+          <strong>Squirrel Mike</strong>
+          <span>@sq_mike</span>
         </div>
-    )
+      </header>
+
+      <aside>
+        <button>
+          Seguir
+        </button>
+      </aside>
+  </article>
+  )
 }
