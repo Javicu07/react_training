7bfb011b0277f2cacc079b09e9a66ef7dc01322a
Javicu07 <javicu89@gmail.com>
2023-10-18 20:27:55 +0200

    twitter_card

--- 00-hello-world/src/App.jsx
+++ 00-hello-world/src/App.jsx
@@ -1,24 +1,10 @@
 import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App () {
   return(
-    <article className='tw-followCard'>
-      <header className='tw-followCard-header'>
-        <img
-          className='tw-followCard-avatar' 
-          alt="squirrel avatar" 
-          src="https://cdn.pixabay.com/photo/2016/03/31/20/31/amazed-1295833_1280.png" />
-        <div className='tw-followCard-info'>
-          <strong>Squirrel Mike</strong>
-          <span className='tw-followCard-infoUserName'>@sq_mike</span>
-        </div>
-      </header>
-
-      <aside>
-        <button className='tw-followCard-button'>
-          Seguir
-        </button>
-      </aside>
-  </article>
+    <>
+      <TwitterFollowCard userName="midudev" name="Miguel A. Duran" />
+    </>
   )
 }
