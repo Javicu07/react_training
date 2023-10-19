aa973dd7fd63f4a1008b16b31b4e6eb1bf5f8146
Javicu07 <javicu89@gmail.com>
2023-10-19 23:23:19 +0200

    managing_lists

--- 00-hello-world/src/App.jsx
+++ 00-hello-world/src/App.jsx
@@ -1,12 +1,42 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Angel Duran',
+    isFollowing: true
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pablo Hernandez',
+    isFollowing: false
+  },
+  {
+    userName: 'PacoHdezs',
+    name: 'Paco Hernandez',
+    isFollowing: false
+  },
+  {
+    userName: 'TMChein',
+    name: 'Tomas Chang',
+    isFollowing: true
+  }
+]
+
 export function App () {
   
   return(
     <section className='App'>
-      <TwitterFollowCard userName="midudev" name="Miguel A. Duran" initialIsFollowing={true}/>
-      <TwitterFollowCard userName="pheralb" name="Pablo Hernandez" />
+      {
+        users.map(({ userName, name, isFollowing}) => {
+            <TwitterFollowCard
+              key={userName}
+              userName={userName} 
+              name={name} 
+              initialIsFollowing={isFollowing} />
+        })
+      }
     </section>
   )
 }
