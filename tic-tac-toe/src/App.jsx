2b02109b8d1961b79e428f94c4ff230c740ced0b
Javicu07 <javicu89@gmail.com>
2023-10-21 23:34:54 +0200

    complete_ttt

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -21,35 +21,86 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [ 0, 1, 2 ],
+  [ 3, 4, 5 ],
+  [ 6, 7, 8 ],
+  [ 0, 3, 6 ],
+  [ 1, 4, 7 ],
+  [ 2, 5, 8 ],
+  [ 0, 4, 8 ],
+  [ 2, 4, 6 ]
+]
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
   
   const [turn, setTurn] = useState(TURNS.X)
-  
-  const updateBoard = (index) => {
-    if ( board[index] ) return //--> return if 'board[index]' contains a value
 
+  // 'null' --> no winner, 'false' --> tie
+  const [winner, setWinner] = useState(null)
+  
+  const checkWinner = (boardToCheck) => {
+    // Review of all winner combinations
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+      // no winner
+      return null
+    }
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  const checkEndGame = (boardToCheck) => {
+    return boardToCheck.every(square => square !== null)
+  }
+
+  const updateBoard = (index) => {
+    if ( board[index] || winner ) return //--> return if 'board[index]' contains a value
+
+    // Update the board
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
 
+    // change turn
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    // checking for winners
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
+    }
   }
 
   return(
     <main className='board'>
       <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Reset del juego</button>
       <section className='game'>
         {
-          board.map((_, index) => {
+          board.map((square, index) => {
             return(
               <Square 
                 key={index}
                 index={index}
                 updateBoard={updateBoard}
               >
-                {index}
+                {square}
               </Square>
             )
           })
@@ -64,6 +115,30 @@ function App() {
           {TURNS.O}
         </Square>
       </section>
+
+      {
+        winner !== null && (
+          <section className='winner'>
+            <div className='text'>
+              <h2>
+                {
+                  winner === false
+                    ? 'Empate'
+                    : 'Ganó:'
+                }
+              </h2>
+
+              <header className='win'>
+                {winner && <Square>{winner}</Square>}
+              </header>
+
+              <footer>
+                <button onClick={resetGame}>Empezar de nuevo</button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
     </main>
   )
 }
