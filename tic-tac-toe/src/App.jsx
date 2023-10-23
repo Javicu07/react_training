9b9643e7c182193bad75fb62c24e614523c6d67f
Javicu07 <javicu89@gmail.com>
2023-10-23 21:24:41 +0200

    separate_components

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -2,36 +2,14 @@ import { useState } from 'react'
 import confetti from 'canvas-confetti'
 import './App.css'
 
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
+import { TURNS } from './constants.js'
 
+import { checkWinnerFrom, checkEndGame } from './logic/board.js'
 
-const Square = ({ children, isSelected, updateBoard, index }) => {
-  const className = `square ${isSelected ? 'is-selected' : ''}`
-  
-  const handleClick = () => {
-    updateBoard(index)
-  }
+import { WinnerModal } from './components/WinnerModal.jsx'
 
-  return(
-    <div onClick={handleClick} className = {className}>
-      {children}
-    </div>
-  )
-}
-
-const WINNER_COMBOS = [
-  [ 0, 1, 2 ],
-  [ 3, 4, 5 ],
-  [ 6, 7, 8 ],
-  [ 0, 3, 6 ],
-  [ 1, 4, 7 ],
-  [ 2, 5, 8 ],
-  [ 0, 4, 8 ],
-  [ 2, 4, 6 ]
-]
+import { BoardGame } from './components/BoardGame.jsx'
+import { TurnChange } from './components/TurnChange'
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
@@ -41,30 +19,10 @@ function App() {
   // 'null' --> no winner, 'false' --> tie
   const [winner, setWinner] = useState(null)
   
-  const checkWinner = (boardToCheck) => {
-    // Review of all winner combinations
-    for (const combo of WINNER_COMBOS) {
-      const [a, b, c] = combo
-      if (
-        boardToCheck[a] &&
-        boardToCheck[a] === boardToCheck[b] &&
-        boardToCheck[a] === boardToCheck[c]
-      ) {
-        return boardToCheck[a]
-      }
-    }
-    // no winner
-    return null
-  }
-
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
-  }
-
-  const checkEndGame = (newBoard) => {
-    return newBoard.every(square => square !== null)
   }
 
   const updateBoard = (index) => {
@@ -80,7 +38,7 @@ function App() {
     setTurn(newTurn)
 
     // checking for winners
-    const newWinner = checkWinner(newBoard)
+    const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
       setWinner(newWinner)
       confetti()
@@ -93,54 +51,12 @@ function App() {
     <main className='board'>
       <h1>Tic Tac Toe</h1>
       <button onClick={resetGame}>Reset del juego</button>
-      <section className='game'>
-        {
-          board.map((_, index) => {
-            return(
-              <Square 
-                key={index}
-                index={index}
-                updateBoard={updateBoard}
-              >
-                {board[index]}
-              </Square>
-            )
-          })
-        }
-      </section>
 
-      <section className='turn'>
-        <Square isSelected={turn === TURNS.X}>
-          {TURNS.X}
-        </Square>
-        <Square isSelected={turn === TURNS.O}>
-          {TURNS.O}
-        </Square>
-      </section>
+      <BoardGame board={board} updateBoard={updateBoard}/>
 
-      {
-        winner !== null && (
-          <section className='winner'>
-            <div className='text'>
-              <h2>
-                {
-                  winner === false
-                    ? 'Empate'
-                    : 'Ganó:'
-                }
-              </h2>
+      <TurnChange turn={turn} />
 
-              <header className='win'>
-                {winner && <Square>{winner}</Square>}
-              </header>
-
-              <footer>
-                <button onClick={resetGame}>Empezar de nuevo</button>
-              </footer>
-            </div>
-          </section>
-        )
-      }
+      <WinnerModal resetGame={resetGame} winner={winner}/>
     </main>
   )
 }
---
+++ tic-tac-toe/src/components/TurnChange.jsx
@@ -0,0 +1,16 @@
+import { TURNS } from "../constants.js"
+
+import { Square } from "./Square.jsx"
+
+export const TurnChange = (turn) => {
+  return(
+    <section className='turn'>
+    <Square isSelected={turn === TURNS.X}>
+      {TURNS.X}
+    </Square>
+    <Square isSelected={turn === TURNS.O}>
+      {TURNS.O}
+    </Square>
+  </section>
+  )
+}
