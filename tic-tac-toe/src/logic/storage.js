db51ddcacf4c4f470ebfd81c01ba7bec79c4bb3c
Javicu07 <javicu89@gmail.com>
2023-10-27 21:43:11 +0200

    tic tac toe finished

--- tic-tac-toe/src/logic/storage.js
+++ tic-tac-toe/src/logic/storage.js
@@ -1,4 +1,9 @@
-export const saveGameToStorage = ({ board, turn}) => {
+export const saveGameToStorage = ({ board, turn }) => {
   window.localStorage.setItem('board', JSON.stringify(board))
-    window.localStorage.setItem('turn', turn)
+  window.localStorage.setItem('turn', turn)
+}
+
+export const resetGameFromStorage = () => {
+  window.localStorage.removeItem('board')
+  window.localStorage.removeItem('turn')
 }
