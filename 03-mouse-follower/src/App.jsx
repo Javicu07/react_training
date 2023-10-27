1a40583ba41fae6509a61c965e55a15434a2c7ec
Javicu07 <javicu89@gmail.com>
2023-10-27 22:38:26 +0200

    adding pointer style

--- 03-mouse-follower/src/App.jsx
+++ 03-mouse-follower/src/App.jsx
@@ -2,17 +2,37 @@ import { useEffect, useState } from "react"
 
 function App() {
   const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0})
 
   useEffect(() => {
-    console.log('efecto')
-  })
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if(enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
   return (
-    <>
-      <h3>Project 3</h3>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
       <button onClick={() => setEnabled(!enabled)}>
         {enabled ? 'Disable' : 'Enable'} Pointer Follower
       </button>
-    </>
+    </main>
   )
 }
 
