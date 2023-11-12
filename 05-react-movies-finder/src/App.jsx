860603175fbef2c337c8b67e3473b564df68719e
Javicu07 <javicu89@gmail.com>
2023-11-12 16:31:05 +0100

    starting useRef

--- 05-react-movies-finder/src/App.jsx
+++ 05-react-movies-finder/src/App.jsx
@@ -1,23 +1,31 @@
-// import { useState } from 'react'
+import { useRef } from 'react'
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
 function App () {
-  const { movies: mappedMovies } = useMovies()
+  const { movies } = useMovies()
+
+  const inputRef = useRef()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const value = inputRef.current.value
+  }
 
   return (
     <div className='page'>
       <header>
         <h1>Buscador de peliculas</h1>
-        <form className='form'>
-          <input placeholder='Avengers, Star Wars, The Matrix, ...' />
+        <form className='form' onSubmit={handleSubmit}>
+          <input ref={inputRef} placeholder='Avengers, Star Wars, The Matrix, ...' />
           <button type='submit'>Buscar</button>
         </form>
       </header>
 
       <main>
-        <Movies movies={mappedMovies} />
+        <Movies movies={movies} />
       </main>
     </div>
   )
