801df1fcfcc99ca1ab3f87bc3369ffd7b1ef888b
Javicu07 <javicu89@gmail.com>
2023-11-13 17:38:38 +0100

    validating input

--- 05-react-movies-finder/src/App.jsx
+++ 05-react-movies-finder/src/App.jsx
@@ -1,27 +1,56 @@
-import { useRef } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
 function App () {
   const { movies } = useMovies()
+  const [query, setQuery] = useState('')
+  const [error, setError] = useState(null)
 
-  const inputRef = useRef()
+  // const inputRef = useRef()
 
   const handleSubmit = (event) => {
     event.preventDefault()
-
-    const value = inputRef.current.value
+    console.log({ query })
+    /* // forma no controlada
+    const { query } = Object.fromEntries(new window.FormData(event.target))
+    console.log(query)
+    */
   }
+
+  const handleChange = (event) => {
+    setQuery(event.target.value)
+  }
+
+  useEffect(() => {
+    if (query === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+
+    if (query.match(/^\d+$/)) {
+      setError('No se puede buscar una película con un número')
+      return
+    }
+
+    if (query.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres')
+      return
+    }
+
+    setError(null)
+  }, [query])
 
   return (
     <div className='page'>
       <header>
         <h1>Buscador de peliculas</h1>
         <form className='form' onSubmit={handleSubmit}>
-          <input ref={inputRef} placeholder='Avengers, Star Wars, The Matrix, ...' />
+          <input onChange={handleChange} value={query} placeholder='Avengers, Star Wars, The Matrix, ...' />
           <button type='submit'>Buscar</button>
         </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
 
       <main>
