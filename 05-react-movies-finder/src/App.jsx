fc4d0dfba2721bbf0996e56f99de30a9ad48691d
Javicu07 <javicu89@gmail.com>
2023-11-14 19:47:03 +0100

    grid responsive

--- 05-react-movies-finder/src/App.jsx
+++ 05-react-movies-finder/src/App.jsx
@@ -6,8 +6,14 @@ import { useMovies } from './hooks/useMovies'
 function useSearch () {
   const [search, updateSearch] = useState('')
   const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
 
   useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
+
     if (search === '') {
       setError('No se puede buscar una película vacía')
       return
