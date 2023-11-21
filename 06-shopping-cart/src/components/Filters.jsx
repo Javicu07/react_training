c4b5452b4c2a16e7377b8dfc719eec97e792da83
Javicu07 <javicu89@gmail.com>
2023-11-21 23:21:04 +0100

    review filters

--- 06-shopping-cart/src/components/Filters.jsx
+++ 06-shopping-cart/src/components/Filters.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react'
 import './Filters.css'
 
-export function Filters () {
+export function Filters ({ onChange }) {
   const [minPrice, setMinPrice] = useState(0)
 
   const handleChangeMinPrice = (event) => {
     setMinPrice(event.target.value)
+
+    onChange(prevState => ({
+      ...prevState,
+      minPrice: event.target.value
+    }))
+  }
+
+  const handleChangeCategory = (event) => {
+    onChange(prevState => ({
+      ...prevState,
+      category: event.target.value
+    }))
   }
 
   return (
@@ -25,7 +37,7 @@ export function Filters () {
 
       <div>
         <label htmlFor='category'>Categoría</label>
-        <select id='category'>
+        <select id='category' onChange={handleChangeCategory}>
           <option value='all'>Todas</option>
           <option value='laptops'>Portátiles</option>
           <option value='smartphones'>Celulares</option>
