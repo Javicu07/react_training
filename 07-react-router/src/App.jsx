1da93a6a1dd5ed5e38411fce6447fee6adb9567a
Javicu07 <javicu89@gmail.com>
2023-12-27 15:02:30 +0100

    creating the router

--- 07-react-router/src/App.jsx
+++ 07-react-router/src/App.jsx
@@ -4,6 +4,17 @@ import { EVENTS } from './consts'
 import { HomePage } from './pages/Home.jsx'
 import { AboutPage } from './pages/About.jsx'
 
+const routes = [
+  {
+    path: '/',
+    Component: HomePage
+  },
+  {
+    path: '/about',
+    Component: AboutPage
+  }
+]
+
 function App () {
   const [currentPath, setCurrentPath] = useState(window.location.pathname)
 
