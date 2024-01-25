ea26971155212c329a76b041305e19f11ac10be7
Javicu07 <javicu89@gmail.com>
2024-01-25 22:50:19 +0100

    creating the table and style

--- prueba-tecnica-empresa-55k/src/App.tsx
+++ prueba-tecnica-empresa-55k/src/App.tsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '../../../../../../../../vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { UsersList } from './components/UsersList.tsx'
 
 function App () {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([])
+  const [showColors, setShowColors] = useState(false)
+
+  const toggleColors = () => {
+    setShowColors(!showColors)
+  }
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=100')
+      .then(async res => await res.json())
+      .then(res => {
+        setUsers(res.results)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => { setCount((count) => count + 1) }}>
-          count is {count}
+      <h1>Prueba técnica</h1>
+      <header>
+        <button onClick={toggleColors}>
+          Colorear filas
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </header>
+      <main>
+        <UsersList showColors={showColors} users={users}/>
+      </main>
     </>
   )
 }
