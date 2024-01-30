ff7f22181a4aef2bf80cc658c8ad7540b0122c0a
Javicu07 <javicu89@gmail.com>
2024-01-30 22:57:11 +0100

    using mui

--- javascript-quiz/.eslintrc.cjs
+++ javascript-quiz/.eslintrc.cjs
@@ -16,18 +16,20 @@ module.exports = {
         '.eslintrc.{js,cjs}'
       ],
       parserOptions: {
-        sourceType: 'script',
-        project: './tsconfig.json'
+        sourceType: 'script'
       }
     }
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    project: './tsconfig.json'
   },
   plugins: [
     'react'
   ],
   rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/react-in-jsx-scope': 'off'
   }
 }
