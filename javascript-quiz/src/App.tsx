630ad85ed374a0040d35300aff477b6c777281c4
Javicu07 <javicu89@gmail.com>
2024-01-30 23:35:58 +0100

    start zustand

--- javascript-quiz/src/App.tsx
+++ javascript-quiz/src/App.tsx
@@ -1,14 +1,22 @@
 import './App.css'
 import { JavaScriptLogo } from './JavaScriptLogo'
-import { Container } from '@mui/material'
+import { Container, Stack, Typography } from '@mui/material'
+import { Start } from './Start'
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 function App () {
   return (
     <main>
-      <Container>
-        <JavaScriptLogo />
-        <h1>Javascript Quiz</h1>
+      <Container maxWidth="sm">
+        <Stack direction="row" gap={2} alignItems="center" justifyContent="center">
+          <JavaScriptLogo />
+          <Typography variant="h2" component="h1">
+            Javascript Quiz
+          </Typography>
+        </Stack>
+
+        <Start />
+
       </Container>
     </main>
   )
--- javascript-quiz/src/main.tsx
+++ javascript-quiz/src/main.tsx
@@ -1,3 +1,5 @@
+// npm install zustand -E
+
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
